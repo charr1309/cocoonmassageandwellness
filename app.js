64b7c90db55d56386d1e106be98066db4d17eb71
charr1309 <ceh710@aol.com>
2024-02-13 21:44:15 -0500

    add wisdom section, change offerings

--- app.js
+++ app.js
@@ -39,4 +39,8 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+function previous(){
+  window.history.go(-1);
 }
