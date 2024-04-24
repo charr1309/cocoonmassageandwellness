f2fde576ab2a5d8f4a189d8375b22bf2e4accdbb
charr1309 <ceh710@aol.com>
2024-04-24 02:21:28 -0400

    update navbar

--- app.js
+++ app.js
@@ -135,5 +135,20 @@ anims.forEach(anim =>{
 
 })
 
+window.onscroll = function(){
+  myFunction()
+};
+var navbar = document.querySelector('.navbar');
+
+
+var sticky = navbar.offsetTop;
+
+function myFunction(){
+  if(window.pageYOffset >= sticky){
+    navbar.classList.add('sticky')
+  }else{
+    navbar.classList.remove('sticky');
+  }
+}
 
 
