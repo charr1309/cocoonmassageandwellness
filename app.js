41a20e2f9aabb6950a8e9325cb9b95e79ae5dc1d
charr1309 <ceh710@aol.com>
2024-05-07 14:56:56 -0400

    update meetshanna

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ let timeDom = document.querySelector('.carousel .time');
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
 let timeRunning = 3000;
-let timeAutoNext = 7000;
+let timeAutoNext = 77000;
 
 nextDom.onclick = function(){
     showSlider('next');    
